a5d77d439f6e0dcfc7073471eee02f134ded7476
piesusoida <134595647+piesusoida@users.noreply.github.com>
2025-03-29 06:21:57 +0100

    Add files via upload

---
+++ kodykreskowe/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace kodykreskowe_cc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(462, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(483, 184);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ciąg bitów:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Suma kontrolna";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(10, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(416, 25);
+            this.label3.TabIndex = 5;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Podaj do 12 cyfr kodu, bez cyfry sumy kontrolnej";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 391);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ kodykreskowe/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+
+namespace kodykreskowe_cc
+{
+    public partial class Form1 : Form
+    {
+        // tablice przechowujace informacje o kodowaniu poszczegolnych cyfr
+        string[] leftA = {"0001101", "0011001", "0010011", "0111101", "0100011",
+         "0110001", "0101111", "0111011", "0110111", "0001011"};
+        string[] leftB = {"0100111", "0110011", "0011011", "0100001", "0011101",
+         "0111001", "0000101", "0010001", "0001001", "0010111"};
+        string[] right = {"1110010", "1100110", "1101100", "1000010", "1011100",
+         "1001110", "1010000", "1000100", "1001000", "1110100"};
+
+        // tablica przechowujaca informacje o kodowaniu pierwszej cyfry
+        int[,]lF = {{0,0,0,0,0,0},
+                    {0,0,1,0,1,1},
+                    {0,0,1,1,0,1},
+                    {0,0,1,1,1,0},
+                    {0,1,0,0,1,1},
+                    {0,1,1,0,0,1},
+                    {0,1,1,1,0,0},
+                    {0,1,0,1,0,1},
+                    {0,1,0,1,1,0},
+                    {0,1,1,0,1,0}};
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // pobranie cyfr podanych przez uzytkownika
+            String str = textBox1.Text.ToString();
+            int[] tmpArr = new int[12] {0,0,0,0,0,0,0,0,0,0,0,0};
+            tmpArr = str.Select(c => c - '0').ToArray();
+            int[] intArr = new int[13];
+            Array.Copy(tmpArr, intArr, tmpArr.Length);
+
+            // obliczanie sumy kontrolnej
+            var digits = intArr;
+            var even = digits.Where((x, i) => i % 2 == 0).Sum();
+            var odd = digits.Where((x, i) => i % 2 != 0).Sum() * 3;
+            int checkDigit = (10 - ((even + odd) % 10)) % 10;
+            intArr[12] = checkDigit; 
+
+            label2.Text = "Suma kontrolna = ";
+            label2.Text += Convert.ToString(checkDigit);
+            label3.Text = "";
+            for ( int i = 0; i < intArr.Length; i++)
+            {
+                label3.Text += Convert.ToString(intArr[i]);
+            }
+
+
+            // tablica przechowujaca zakodowane cyfry kodu kreskowego
+            StringBuilder sb = new StringBuilder();
+            // bity startu
+            sb.Append("101");
+            int first = intArr[0];
+            // kodowanie 6 cyfr lewej strony kodu kreskowego
+            for(int i = 1; i < 7; i++)
+            {
+                int num = intArr[i];
+
+                if (lF[first,i-1] == 0)
+                {
+                    sb.Append(leftA[num]);
+                }
+                if (lF[first, i-1] == 1)
+                {
+                    sb.Append(leftB[num]);
+                }
+            }
+            // bity srodkowego znacznika
+            sb.Append("01010");
+            // kodowanie 6 cyfr prawej strony kodu kreskowego
+            for (int i = 7; i < 13; i++)
+            {          
+                sb.Append(right[intArr[i]]);
+            }
+            // bity stopu
+            sb.Append("101");
+
+            label1.Text = Convert.ToString(sb);
+
+            int width = 500;
+            int height = 500;
+
+            Bitmap bmp = new Bitmap(width, height);
+            System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            System.Drawing.SolidBrush brush2 = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            string strtemp = sb.ToString();
+            float xPosition = 0;
+            float yStart = 0;
+            float xEnd = 0;
+            float lineWidth = 3;
+
+            // tworzenie grafiki na podstawie ciagu bitow
+            Graphics g = Graphics.FromImage(bmp);
+            for (int i = 0; i < sb.Length; i++)
+            {
+                // Rysowanie czarnych paskow
+                if (strtemp[i] == '1')
+                {
+                        g.FillRectangle(brush, xPosition, yStart, lineWidth, height);
+                }
+
+                xPosition += lineWidth;
+                xEnd = xPosition;
+            }
+            // zapisanie grafiki do pliku png
+            g.Dispose();
+
+            String filename = "barcode.png";
+            bmp.Save(filename);
+            pictureBox1.Image = Image.FromFile(filename);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
